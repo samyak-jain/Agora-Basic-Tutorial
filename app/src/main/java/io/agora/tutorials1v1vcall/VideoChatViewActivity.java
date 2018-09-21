77487b5d94783f3350b758d30628306e96c6a7f6
Samyak Jain <samtan106@gmail.com>
2018-09-21 22:45:24 +0700

    Initial Commit

---
+++ app/src/main/java/io/agora/tutorials1v1vcall/VideoChatViewActivity.java
@@ -0,0 +1,143 @@
+package io.agora.tutorials1v1vcall;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+import io.agora.rtc.video.VideoEncoderConfiguration;
+
+public class VideoChatViewActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = VideoChatViewActivity.class.getSimpleName();
+
+    private RtcEngine mRtcEngine; // Tutorial Step 1
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() { // Tutorial Step 1
+        @Override
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) { // Tutorial Step 5
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) { // Tutorial Step 7
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onRemoteUserLeft();
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat_view);
+
+            initAgoraEngineAndJoinChannel();
+
+    }
+
+    private void initAgoraEngineAndJoinChannel() {
+        initializeAgoraEngine();     // Tutorial Step 1
+        setupVideoProfile();         // Tutorial Step 2
+        setupLocalVideo();           // Tutorial Step 3
+        joinChannel();               // Tutorial Step 4
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        leaveChannel();
+        RtcEngine.destroy();
+        mRtcEngine = null;
+    }
+
+
+    // Tutorial Step 8
+    public void onSwitchCameraClicked(View view) {
+        mRtcEngine.switchCamera();
+    }
+
+    // Tutorial Step 6
+    public void onEncCallClicked(View view) {
+        finish();
+    }
+
+    // Tutorial Step 1
+    private void initializeAgoraEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    // Tutorial Step 2
+    private void setupVideoProfile() {
+        mRtcEngine.enableVideo();
+
+//      mRtcEngine.setVideoProfile(Constants.VIDEO_PROFILE_360P, false); // Earlier than 2.3.0
+        mRtcEngine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(VideoEncoderConfiguration.VD_640x360, VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT));
+    }
+
+    // Tutorial Step 3
+    private void setupLocalVideo() {
+        FrameLayout container = (FrameLayout) findViewById(R.id.local_video_view_container);
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext());
+        surfaceView.setZOrderMediaOverlay(true);
+        container.addView(surfaceView);
+        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, 0));
+    }
+
+    // Tutorial Step 4
+    private void joinChannel() {
+        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0); // if you do not specify the uid, we will generate the uid for you
+    }
+
+    // Tutorial Step 5
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+
+        if (container.getChildCount() >= 1) {
+            return;
+        }
+
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext());
+        container.addView(surfaceView);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
+
+        surfaceView.setTag(uid); // for mark purpose
+        View tipMsg = findViewById(R.id.quick_tips_when_use_agora_sdk); // optional UI
+        tipMsg.setVisibility(View.GONE);
+    }
+
+    // Tutorial Step 6
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();
+    }
+
+    // Tutorial Step 7
+    private void onRemoteUserLeft() {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+        container.removeAllViews();
+
+        View tipMsg = findViewById(R.id.quick_tips_when_use_agora_sdk); // optional UI
+        tipMsg.setVisibility(View.VISIBLE);
+    }
+}
